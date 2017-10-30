e6d4962ffb8fecc9085577d024d6b56489c60d0f
linhongyue <linhongyue19980405@gmail.com>
2017-10-30 14:27:42 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner in= new Scanner(System.in);
+	    int a;
+	    a=in.nextInt();
+	    if(a%2==0)
+	       System.out.print("偶");
+        if(a%2==1)
+           System.out.print("奇");
+    }
+}
